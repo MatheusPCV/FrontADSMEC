0f527d60972ca0ac888d89fefab461e7aded0116
MatheusPCV <matheus.p.vieiraa@gmail.com>
2023-11-30 20:57:48 -0300

    s

--- app/App.js
+++ app/App.js
@@ -21,15 +21,14 @@ const generateRandomData = () => {
 
   for (let i = 0; i < 10; i++) {
     const randomValue = getRandomFloat(20, 30);
-    const date = new Date(currentDate.getTime() + i * 30 * 60 * 1000); // Adiciona 30 minutos a cada iteração
+    const date = new Date(currentDate.getTime() + i * 30 * 60 * 1000);
     simulatedData.push({
       date: date.toISOString(),
       value: parseFloat(randomValue),
     });
 
-    // Simulação do ventilador ligado por 4 horas no total
     if (i >= 2 && i < 8) {
-      fanOnTime += 0.5; // A cada 30 minutos
+      fanOnTime += 0.5;
     }
   }
 
@@ -62,38 +61,39 @@ const App = () => {
       <Button title="Fetch Data" onPress={fetchData} />
 
       {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#000" />
       ) : (
         <View style={styles.chartContainer}>
           <View style={styles.bigContainer}>
+            <View style={styles.timeContainer}>
+              <Icon
+                name="access-time"
+                size={60}
+                color="#000"
+                style={styles.timeImg}
+              />
+              <Text style={styles.timeTitle}>Horas Ligadas</Text>
+              <Text style={styles.timeText}>
+                {fanData.fanOnTime.toFixed(2)} Horas
+              </Text>
+            </View>
+
+            <View style={styles.gap}></View>
+
             <View style={styles.statusContainer}>
               <Icon
                 style={styles.statusImg}
                 name={fanData.fanStatus ? "power" : "power-off"}
                 size={60}
-                color={fanData.fanStatus ? "black" : "black"}
+                color={fanData.fanStatus ? "#000" : "#000"}
               />
               <Text style={styles.statusTitle}>Status</Text>
               <Text style={styles.statusText}>
                 {fanData.fanStatus ? "Ligado" : "Desligado"}
               </Text>
             </View>
-
-            <View style={styles.timeContainer}>
-              <Icon
-                name="access-time"
-                size={60}
-                color="#FFD700"
-                style={styles.timeImg}
-              />
-              <Text style={styles.timeTitle}>Horas Ligadas</Text>
-              <Text style={styles.timeText}>
-                {fanData.fanOnTime.toFixed(2)} horas
-              </Text>
-            </View>
           </View>
 
-          {/* Gráfico de Temperatura */}
           <LineChart
             data={{
               labels: fanData.data.map((item) =>
@@ -113,11 +113,12 @@ const App = () => {
             yAxisLabel="°C"
             yAxisSuffix=""
             chartConfig={{
-              backgroundGradientFrom: "#fff",
-              backgroundGradientTo: "#fff",
+              backgroundGradientFrom: "#EAEAEA",
+              backgroundGradientTo: "#EAEAEA",
               decimalPlaces: 2,
-              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              color: (opacity = 1) => `rgba(255, 223, 0, ${opacity})`, // Cor amarela
+              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Cor preta
+              gridColor: "#FFFFFF",
               style: {
                 borderRadius: 16,
               },
@@ -168,17 +169,23 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     height: 200,
     width: 200,
-    backgroundColor: "#FFD700",
+    backgroundColor: "black",
+    borderRadius: 16,
+    overflow: "hidden",
   },
   statusImg: {
     paddingRight: 90,
+    color: "#FFD700",
   },
   statusTitle: {
     fontSize: 15,
+    color: "#FFD700",
   },
   statusText: {
     fontSize: 20,
     marginLeft: 10,
+    color: "#FFD700",
+    fontWeight: "bold",
   },
   timeContainer: {
     paddingTop: 20,
@@ -187,19 +194,22 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     height: 200,
     width: 200,
-    backgroundColor: "black",
+    backgroundColor: "#FFD700",
+    borderRadius: 16,
+    overflow: "hidden",
   },
   timeImg: {
     paddingRight: 90,
   },
   timeTitle: {
-    fontSize: "15",
-    color: "#FFD700",
+    fontSize: 15,
+    color: "#000",
   },
   timeText: {
     fontSize: 20,
     marginLeft: 10,
-    color: "#FFD700",
+    color: "#000",
+    fontWeight: "bold",
   },
 });
 
