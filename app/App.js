8fea5f5072f4f7568fda5abe3cfb378e4cad17c5
Vincenzo140 <vincenzo.amendola141@gmail.com>
2023-12-04 20:02:12 -0300

    commit

--- app/App.js
+++ app/App.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  ActivityIndicator,
-  ScrollView,
-} from "react-native";
-import { LineChart } from "react-native-chart-kit";
+import { View, Text, StyleSheet, Button, ActivityIndicator, ScrollView } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 const getRandomFloat = (min, max) => {
@@ -21,15 +13,14 @@ const generateRandomData = () => {
 
   for (let i = 0; i < 10; i++) {
     const randomValue = getRandomFloat(20, 30);
-    const date = new Date(currentDate.getTime() + i * 30 * 60 * 1000); // Adiciona 30 minutos a cada iteração
+    const date = new Date(currentDate.getTime() + i * 30 * 60 * 1000);
     simulatedData.push({
       date: date.toISOString(),
       value: parseFloat(randomValue),
     });
 
-    // Simulação do ventilador ligado por 4 horas no total
     if (i >= 2 && i < 8) {
-      fanOnTime += 0.5; // A cada 30 minutos
+      fanOnTime += 0.5;
     }
   }
 
@@ -60,71 +51,42 @@ const App = () => {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.header}>API Fetcher</Text>
       <Button title="Fetch Data" onPress={fetchData} />
+      <View style={styles.gap}></View>
 
       {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#000" />
       ) : (
-        <View style={styles.chartContainer}>
-          <View style={styles.bigContainer}>
+        <View style={styles.bigContainer}>
+          <View style={styles.blockContainer}>
+            <View style={styles.timeContainer}>
+              <Icon name="access-time" size={60} color="#000" style={styles.timeImg} />
+              <Text style={styles.timeTitle}>Horas Ligadas</Text>
+              <Text style={styles.timeText}>{fanData.fanOnTime.toFixed(2)} Horas</Text>
+            </View>
+          </View>
+
+          <View style={styles.blockContainer}>
+            <View style={styles.temperatureContainer}>
+              <Icon name="thermostat" size={60} color="#000" style={styles.temperatureImg} />
+              <Text style={styles.temperatureTitle}>Temperatura Atual</Text>
+              <Text style={styles.temperatureText}>
+                {fanData.data.length > 0 ? fanData.data[0].value.toFixed(2) : "-"} °C
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.blockContainer}>
             <View style={styles.statusContainer}>
               <Icon
                 style={styles.statusImg}
                 name={fanData.fanStatus ? "power" : "power-off"}
                 size={60}
-                color={fanData.fanStatus ? "black" : "black"}
+                color={fanData.fanStatus ? "#000" : "#000"}
               />
               <Text style={styles.statusTitle}>Status</Text>
-              <Text style={styles.statusText}>
-                {fanData.fanStatus ? "Ligado" : "Desligado"}
-              </Text>
-            </View>
-
-            <View style={styles.timeContainer}>
-              <Icon
-                name="access-time"
-                size={60}
-                color="#FFD700"
-                style={styles.timeImg}
-              />
-              <Text style={styles.timeTitle}>Horas Ligadas</Text>
-              <Text style={styles.timeText}>
-                {fanData.fanOnTime.toFixed(2)} horas
-              </Text>
+              <Text style={styles.statusText}>{fanData.fanStatus ? "Ligado" : "Desligado"}</Text>
             </View>
           </View>
-
-          {/* Gráfico de Temperatura */}
-          <LineChart
-            data={{
-              labels: fanData.data.map((item) =>
-                new Date(item.date).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })
-              ),
-              datasets: [
-                {
-                  data: fanData.data.map((item) => item.value),
-                },
-              ],
-            }}
-            width={350}
-            height={200}
-            yAxisLabel="°C"
-            yAxisSuffix=""
-            chartConfig={{
-              backgroundGradientFrom: "#fff",
-              backgroundGradientTo: "#fff",
-              decimalPlaces: 2,
-              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            bezier
-            style={styles.chartStyle}
-          />
         </View>
       )}
     </ScrollView>
@@ -133,10 +95,13 @@ const App = () => {
 
 const styles = StyleSheet.create({
   bigContainer: {
-    display: "flex",
     flexDirection: "row",
-    justifyContent: "center",
-    gap: 30,
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+  },
+  blockContainer: {
+    width: "45%", // Ajuste conforme necessário
+    marginBottom: 20,
   },
   container: {
     flexGrow: 1,
@@ -150,56 +115,75 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontWeight: "bold",
   },
-
-  chartContainer: {
-    display: "flex",
-    marginTop: 20,
+  temperatureContainer: {
+    paddingTop: 20,
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
-  },
-  chartStyle: {
-    marginVertical: 8,
+    height: 200,
+    backgroundColor: "#FFD700",
     borderRadius: 16,
+    overflow: "hidden",
+  },
+  temperatureImg: {
+    paddingRight: 90,
+  },
+  temperatureTitle: {
+    fontSize: 15,
+    color: "#000",
+  },
+  temperatureText: {
+    fontSize: 20,
+    marginLeft: 10,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  gap: {
+    marginBottom: 20,
   },
   statusContainer: {
     paddingTop: 20,
     flexDirection: "column",
     alignItems: "center",
-    marginBottom: 20,
     height: 200,
-    width: 200,
-    backgroundColor: "#FFD700",
+    backgroundColor: "black",
+    borderRadius: 16,
+    overflow: "hidden",
   },
   statusImg: {
     paddingRight: 90,
+    color: "#FFD700",
   },
   statusTitle: {
     fontSize: 15,
+    color: "#FFD700",
   },
   statusText: {
     fontSize: 20,
     marginLeft: 10,
+    color: "#FFD700",
+    fontWeight: "bold",
   },
   timeContainer: {
     paddingTop: 20,
     flexDirection: "column",
     alignItems: "center",
-    marginBottom: 20,
     height: 200,
-    width: 200,
-    backgroundColor: "black",
+    backgroundColor: "#FFD700",
+    borderRadius: 16,
+    overflow: "hidden",
   },
   timeImg: {
     paddingRight: 90,
   },
   timeTitle: {
-    fontSize: "15",
-    color: "#FFD700",
+    fontSize: 15,
+    color: "#000",
   },
   timeText: {
     fontSize: 20,
     marginLeft: 10,
-    color: "#FFD700",
+    color: "#000",
+    fontWeight: "bold",
   },
 });
 
